88b31996ada953b6def16b1f768e2afc01dcc0e0
lamarjones112881-bit <lamarjones112881@gmail.com>
2026-06-14 18:54:49 -0400

    Add files via upload

---
+++ deploy/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NFLParlayModel from './NFLParlayModel.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NFLParlayModel />
+  </React.StrictMode>,
+)
